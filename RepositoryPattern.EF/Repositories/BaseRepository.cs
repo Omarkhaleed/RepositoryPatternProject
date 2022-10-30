e965f7b0484c84da9b44456f63fe56bb3d643910
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-10-30 09:31:35 +0200

    Add files via upload

---
+++ RepositoryPattern.EF/Repositories/BaseRepository.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPattern.API.Contants;
+using RepositoryPattern.core.Interfaces;
+using RepositoryPattern.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.EF.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+
+          return   await _context.Set<T>().FindAsync(id);
+
+        }
+
+    
+        public T Find(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return query.SingleOrDefault(criteria);
+
+
+        }
+
+        public  async Task<T> FindAsync(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return await query.SingleOrDefaultAsync(criteria);
+
+        }
+        public IEnumerable <T> FindAll(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return query.Where(criteria).ToList();
+
+
+        }
+
+        public async  Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return  await query.Where(criteria).ToListAsync();
+        }
+
+        public IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, int skip, int take)
+        {
+            return _context.Set<T>().Where(criteria).Skip(skip).Take(take).ToList();
+        }
+
+        public IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, int? skip, int? take,
+            Expression<Func<T, object>> orderBy = null, string orderByDirection = OrderBy.Ascending)
+        {
+            IQueryable<T> query = _context.Set<T>().Where(criteria);
+
+            if (skip.HasValue)
+                query = query.Skip(skip.Value);
+
+            if (take.HasValue)
+                query = query.Take(take.Value);
+
+            if (orderBy != null)
+            {
+                if (orderByDirection == OrderBy.Ascending)
+                    query = query.OrderBy(orderBy);
+                else
+                    query = query.OrderByDescending(orderBy);
+            }
+
+            return query.ToList();
+        }
+
+
+
+        public T Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            return entity;
+        }
+
+
+
+    }
+}
