ff8590009e8ae55d35219d770bfabe024fbe4eb4
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-10-30 09:29:37 +0200

    Add files via upload

---
+++ RepositoryPattern.API/Controllers/AuthorsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPattern.core.Interfaces;
+using RepositoryPattern.core.Models;
+using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+         //if I use Repositories
+        //public readonly IBaseRepository<Author> _authorsRepository;
+
+        //public AuthorsController(IBaseRepository<Author> authorsRepository)
+        //{
+        //    _authorsRepository = authorsRepository;
+        //}
+
+        private readonly IUnitOfWork _unitOfWork;
+        public AuthorsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        //[HttpGet]
+        //public IActionResult GetById()
+        //{
+        //    return Ok(_authorsRepository.GetById(1));
+        //}
+
+        [HttpGet]
+        public IActionResult GetById()
+        {
+            return Ok(_unitOfWork.Authors.GetById(1));
+        }
+
+        [HttpGet("GetIdByAsync")]
+        public async Task<IActionResult> GetByIdAsync()
+        {
+            return Ok(await _unitOfWork.Authors.GetByIdAsync(1));
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(_unitOfWork.Authors.GetAll());
+        }
+    }
+}
---
+++ RepositoryPattern.API/Controllers/BooksController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPattern.API.Contants;
+using RepositoryPattern.core.Interfaces;
+using RepositoryPattern.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        //public readonly IBaseRepository<Book> _unitOfWork.Books;
+
+        //public BooksController(IBaseRepository<Book> booksRepository)
+        //{
+        //    _unitOfWork.Books = booksRepository;
+        //}
+
+
+        private readonly IUnitOfWork _unitOfWork;
+        public BooksController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(_unitOfWork.Books.GetAll());
+        }
+
+        [HttpGet("GetAllAsync")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok(await _unitOfWork.Books.GetAllAsync());
+        }
+
+
+        [HttpGet("GetById")]
+        public IActionResult GetById()
+        {
+            return Ok(_unitOfWork.Books.GetById(1));
+        }
+
+
+        [HttpGet("GetByIdAsync")]
+        public async Task<IActionResult> GetByIdAsync()
+        {
+            return  Ok(await _unitOfWork.Books.GetByIdAsync(1));
+        }
+
+
+
+        [HttpGet("GetByName")]
+        public IActionResult GetByName()
+        {
+
+            return Ok(_unitOfWork.Books.Find(P => P.Title == "Time", new[] { "Author" }));
+
+        }
+
+        [HttpGet("GetByNameAsync")]
+        public  async Task<IActionResult> GetByNameAsync()
+        {
+
+            return  Ok(await _unitOfWork.Books.FindAsync(P => P.Title == "Time", new[] { "Author" }));
+
+        }
+
+        [HttpGet("GetAllByName")]
+        public IActionResult GetAllByName()
+        {
+
+            return Ok(_unitOfWork.Books.FindAll(P => P.Title.Contains("Book"), new[] {"Author"}));
+
+        }
+
+
+        [HttpGet("GetAllByNameAsync")]
+        public async Task<IActionResult> GetAllByNameAsync()
+        {
+
+            return Ok(await _unitOfWork.Books.FindAllAsync(P => P.Title.Contains("Book"), new[] { "Author" }));
+
+        }
+
+        [HttpGet("GetOrdered")]
+        public IActionResult GetOrdered()
+        {
+            return Ok(_unitOfWork.Books.FindAll(b => b.Title.Contains("New Book"), null, null, b => b.Id, OrderBy.Descending));
+        }
+
+        [HttpPost("AddOne")]
+        public IActionResult AddOne()
+        {
+            var book = _unitOfWork.Books.Add(new Book { Title = "TestBook", AuthorId = 1 });
+            _unitOfWork.Complete();
+            return Ok(book);
+        }
+
+    }
+}
